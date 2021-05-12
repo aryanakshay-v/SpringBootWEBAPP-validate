7e26450756d6bbffdd8c03518abfd62aa8b9be47
Aryan Akshay <varyanakshay@gmail.com>
2021-05-12 15:04:43 +0530

    Validate INPUT FORM

---
+++ src/main/java/com/example/demo/SpringbootWebappValidateInputFormApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebappValidateInputFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebappValidateInputFormApplication.class, args);
+	}
+
+}
